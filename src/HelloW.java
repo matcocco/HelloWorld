c4c9286ed5f27e9afef55228bf908a50feb37a9a
Mateus <matcocco@hotmail.com>
2017-05-24 13:25:31 -0400

    first commit of project

---
+++ src/HelloW.java
@@ -0,0 +1,12 @@
+
+public class HelloW {
+	/*this is a comment
+	 * Author: MC
+	 * Date: 5/24/17
+	 * this program is printing HelloWorld
+	 */
+	public static void main (String [] args){
+		//In line comment
+		System.out.println("Hello World");
+	}
+}
